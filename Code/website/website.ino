b22ac2dbde9ca35358009e9051e4c58d43e00d22
Xheis <facebook.xheis@gmail.com>
2018-10-26 12:08:34 +1100

    Motors working

--- Code/website/website.ino
+++ Code/website/website.ino
@@ -1,57 +1,34 @@
-
+#include <string.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include "website.h"
+
 
 /* Set these to your desired credentials. */
 const char *ssid = "LitterBot";
 const char *password = "LitterBot";
 
 
-//These are our possible options for the dc motors
-//new espduino 2 motors&16 servos
-#define PinPWMA 14
-#define PinPWMB 12
-#define PinDirA 15
-#define PinDirB 13
+//These are our dc motor pins
 
-//old espduino 2 motors&16 servos
-/*#define PinPWMA 3
-#define PinPWMB 12
-#define PinDirA 1
-#define PinDirB 13*/
-
-//nodemcu dev kit
-/*#define PinPWMA 5
-  #define PinPWMB 4
-  #define PinDirA 0
-  #define PinDirB 2*/
-
-
-
+#define PinPWMA D1
+#define PinPWMB D2
+#define PinDirA1 D4
+#define PinDirA2 D6
+#define PinDirB1 D3
+#define PinDirB2 D5
 
 
 ESP8266WebServer server(80);
-  
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
-   connected to this access point to see it.
-*/
-void handleRoot() { 
- String s = ourWebsite; //Read HTML contents
- server.send(200, "text/html", s); //Send web page
-//server.send(200, "text/html", "<form action=\"/forward\" method=\"POST\"><input type=\"submit\" style=\"background-color: #c14547;\" value=\"\&#x2191\"></form>");
-}
 
-/*
-void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
-}*/
-
-void setup() {
+// Setup is where we "set up" our program. We start the website, send out the wifi, and get ready to accept button clicks!
+void setup() 
+{
   delay(1000);
   ESP.eraseConfig();
   delay(1000);
+  initMotors();
   Serial.begin(115200);
   Serial.println();
   Serial.print("Configuring access point...");
@@ -62,50 +39,33 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(myIP);
   server.on("/", handleRoot);
-  server.on("/forward", HTTP_POST, handleForward);  // If we go to 192.168.4.1/forward, we'll run the handleForward() code
-  server.on("/back", HTTP_POST, handleBack);
-  server.on("/left", HTTP_POST, handleLeft);
-  server.on("/right", HTTP_POST, handleRight);
+  server.on("/forward", HTTP_POST, Forward);  // If we go to 192.168.4.1/forward, we'll run the handleForward() code
+  server.on("/back", HTTP_POST, Back);
+  server.on("/left", HTTP_POST, Left);
+  server.on("/right", HTTP_POST, Right);
   server.begin();
   Serial.println("HTTP server started");
 }
 
+
+//This is our loop. This is what we'll do forever until we turn the controller off!
 void loop() {
   server.handleClient();
 }
 
-void handleForward()
-{
-  Forward();
-  Serial.println("Forward!");
-  // Tell our website to take us back to the controller homepage
-  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
-  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+  
+/* Just a little test message.  Go to http://192.168.4.1 in a web browser
+   connected to this access point to see it.
+*/
+void handleRoot() { 
+ String s = ourWebsite; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+//server.send(200, "text/html", "<form action=\"/forward\" method=\"POST\"><input type=\"submit\" style=\"background-color: #c14547;\" value=\"\&#x2191\"></form>");
 }
 
-void handleBack()
-{
-  Back();
-  Serial.println("Back!");
-  // Tell our website to take us back to the controller homepage
-  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
-  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
-}
 
-void handleRight()
+void ResetButton()
 {
-  Right();
-  Serial.println("Right!");
-  // Tell our website to take us back to the controller homepage
-  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
-  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
-}
-
-void handleLeft()
-{
-  Left();
-  Serial.println("Left!");
-  // Tell our website to take us back to the controller homepage
   server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
 }
@@ -113,56 +73,145 @@ void handleLeft()
 
 void Forward()
 {
-  digitalWrite(PinDirA, LOW); 
-  digitalWrite(PinDirB, HIGH);
-  StartMotors();
+  SetDirection("forward");
+  StartMotors(100,1);
+  ResetButton();
 }
 
 void Back()
 {
-  digitalWrite(PinDirA, HIGH); 
-  digitalWrite(PinDirB, LOW);
-  StartMotors();
+  SetDirection("back");
+  StartMotors(100,1);
+  ResetButton();
 }
 
 void Left()
 {
-  digitalWrite(PinDirA, LOW); 
-  digitalWrite(PinDirB, LOW);
-  StartMotors();
+  SetDirection("left");
+  StartMotors(50,1);
+  ResetButton();
 }
 
 void Right()
 {
-  digitalWrite(PinDirA, HIGH); 
-  digitalWrite(PinDirB, HIGH);
-  StartMotors();
+  SetDirection("right");
+  StartMotors(50,1);
+  ResetButton();
 }
 
-void StartMotors()
+void StartMotors(int speed, float timeInSeconds)
 {
-  analogWrite(PinPWMA, 1023);
-  analogWrite(PinPWMB, 1023);
-  delay(1000);
+  if (speed < 0)
+  {
+    speed = 0;
+  }
+  if (speed > 100)
+  {
+    speed = 1023;
+  }
+  int PWMspeed = speed*(1023.0/100.0);
+  int delayTime = timeInSeconds*1000;
+  Serial.println(PWMspeed);
+  Serial.println(timeInSeconds);
+  analogWrite(PinPWMA, PWMspeed);
+  analogWrite(PinPWMB, PWMspeed);
+  delay(delayTime);
   analogWrite(PinPWMA, 0);
   analogWrite(PinPWMB, 0);
 }
 
 void initMotors()
 {
-  //set motors to outputs
+  //Tells our arduino that our 2 PWM pins & 4 Direction pins will be OUTPUTING a signal, not RECIEVING a signal
   pinMode(PinPWMA, OUTPUT); 
   pinMode(PinPWMB, OUTPUT);
-
-//set motor directions to outputs
-  pinMode(PinDirA, OUTPUT); 
-  pinMode(PinDirB, OUTPUT);
-
-//set directions
-  digitalWrite(PinDirA, HIGH); 
-  digitalWrite(PinDirB, HIGH);
+  pinMode(PinDirA1, OUTPUT); 
+  pinMode(PinDirA2, OUTPUT); 
+  pinMode(PinDirB1, OUTPUT);
+  pinMode(PinDirB2, OUTPUT); 
 
 //set speeds to 0
   digitalWrite(PinPWMA, LOW); 
   digitalWrite(PinPWMB, LOW);
+
+
+//set directions
+  SetDirection("forward"); //set our direction so we're ready to go!
+}
+
+
+
+void SetDirection(char* direction)
+{
+
+  if (direction == "forward")
+  {
+     LeftMotorMode("clockwise");
+     RightMotorMode("anticlockwise");
+  }
+  else if (direction == "back")
+  {
+     LeftMotorMode("anticlockwise");
+     RightMotorMode("clockwise");
+  }
+  else if (direction == "left")
+  {
+     LeftMotorMode("clockwise");
+     RightMotorMode("clockwise");
+  }
+  else if (direction == "right")
+  {
+     LeftMotorMode("anticlockwise");
+     RightMotorMode("anticlockwise");
+  }
+}
+
+// LeftMotorMode(mode) is our function that will make our left motor turn clockwise, anticlockwise or stop all together.
+// It does this by taking what we tell it, e.g. LeftMotorMode("clockwise"), and it 
+/*
+ * Truth table from the manufacturer
+          A1  A2
+          ______
+STOP    |  0   0
+FORWARD |  1   0
+REVERSE |  0   1
+
+*/
+void LeftMotorMode(char* mode)
+{
+    if ( mode == "clockwise") //clockwise is "FORWARD", so we need A1 = 1, A2 = 0
+     {
+        digitalWrite(PinDirA1, HIGH); //A1 = 1
+        digitalWrite(PinDirA2, LOW);  //A2 = 0
+     } 
+     if (mode == "anticlockwise") //anticlockwise is "REVERSE", so we need A1 = 0, A2 = 1
+     {
+        digitalWrite(PinDirA1, LOW);  //A1 = 0
+        digitalWrite(PinDirA2, HIGH); //A2 = 1
+     }
+     else if (mode == "stop")
+     {
+        digitalWrite(PinDirA1, LOW); //A1 = 0
+        digitalWrite(PinDirA2, LOW); //A2 = 0
+     }
+}
+
+/// Same as above, but for B
+void RightMotorMode(char* mode)
+{
+    if ( mode == "clockwise")
+     {
+        digitalWrite(PinDirB1, HIGH); 
+        digitalWrite(PinDirB2, LOW); 
+     } 
+     else if (mode == "anticlockwise")
+     {
+        digitalWrite(PinDirB1, LOW); 
+        digitalWrite(PinDirB2, HIGH); 
+     }
+     else if (mode == "stop")
+     {
+        digitalWrite(PinDirB1, LOW); 
+        digitalWrite(PinDirB2, LOW); 
+     }
 }
